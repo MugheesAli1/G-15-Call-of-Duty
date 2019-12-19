3fab7d4dcd2d3b28856bf725b8da116726cbfa63
MugheesAli1 <58977374+MugheesAli1@users.noreply.github.com>
2019-12-19 00:35:09 -0800

    G15 Commit

---
+++ Assets/G15_Scripts/G15_L2_AmmadMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEditor;
+
+public class G15_L2_AmmadMain : MonoBehaviour
+{
+    public Button HomeButton;
+    // Start is called before the first frame update
+    public Button inputBtn;
+    public Text inputText;
+    string InputString;
+    void Start()
+    {
+        HomeButton.onClick.AddListener(homefunction);
+        inputBtn.onClick.AddListener(NextScene);
+    }
+    private void homefunction()
+    {
+        SceneManager.LoadScene("G15_MainMenu");
+
+    }
+    private void NextScene()
+    {
+        InputString = inputText.text.Trim();
+        Regex regex = new Regex("^[x-y]*$");
+        if (!regex.IsMatch(InputString))
+        {
+            inputText.text = String.Empty;
+            EditorUtility.DisplayDialog("Invalid","Enter Only x or y","Thanks");
+            
+            return;
+        }
+        else
+        SceneManager.LoadScene("G15_L2_AmmadMachine");
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetString("Input", inputText.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
